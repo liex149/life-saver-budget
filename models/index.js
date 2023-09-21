853f943394d73171448227911e29c2c5c1a74364
unknown <leex3683@umn.edu>
2023-09-20 21:08:21 -0500

    added index model relationships

--- models/index.js
+++ models/index.js
@@ -1,4 +1,32 @@
 const User = require("./User");
-const Transaction = require("./Transaction")
-const Category = require("./Category")
-module.exports = { User }
+const Transaction = require("./Transaction");
+const Category = require("./Category");
+
+User.hasMany(Transaction, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Category, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Transaction.belongsTo(User,{
+    foreignKey: "user_id",
+})
+
+Category.belongsTo(User, {
+    foreignKey: "user_id",
+})
+
+Category.hasMany(Transaction,{
+    foreignKey: "category_id",
+    onDelete: "CASCADE",
+})
+
+Transaction.belongsTo(Category,{
+    foreignKey: "category_id",
+})
+
+module.exports = { User, Transaction, Category };

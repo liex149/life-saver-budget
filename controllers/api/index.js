b68cd94e5343dcb99791f846ecaf29522f6979fd
Vilson Apostolovski II <vilsonapo@me.com>
2023-09-21 19:50:57 -0400

    Added .env example and updated api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const transactionRoutes = require('./transactionRoutes')
 
 router.use('/user', userRoutes);
+router.use('/transaction', transactionRoutes);
 
 module.exports = router;

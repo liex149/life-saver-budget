c6d31629b57a6dc3583e6018142eca80a92c2c0c
Linh Lien <lienx149@gmail.com>
2023-09-21 19:26:36 -0500

    logout button working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,5 +26,22 @@ router.all("/login", (req, res) => {
   res.render("login");
 });
 
+router.delete('/delete/:id', withAuth, async (req, res) => {
+  try {
+    const deleteBlogPost = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
+    if (!deleteBlogPost) {
+      res.status(404).json({ message: 'No blog post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(deleteBlogPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;

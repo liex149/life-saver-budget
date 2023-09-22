53942c246b981dc4502f81de374de17ea3114974
unknown <leex3683@umn.edu>
2023-09-21 19:16:07 -0500

    fixed login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const transactionRoutes = require('./transactionRoutes')
 
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/transaction', transactionRoutes);
 
 module.exports = router;
